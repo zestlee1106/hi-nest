3a260f91df98a989db04e7ab61e6db5f6719ed67
이영우 <zestlee1106@gmail.com>
2024-07-13 22:09:19 +0900

    add: 서비스 추가 및 entity 추가해서 type 연동
    - 서비스를 추가해서 컨트롤러에 의존성 주입을 시켜주는 이유는, 서비스 로직을 분리해서 관심사를 분리시키기 위함이다
    - 컨트롤러는 라우팅만 해주고, 서비스에서는 진짜 비즈니스 로직을 할 수 있도록

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,34 +6,32 @@ import {
   Param,
   Patch,
   Post,
-  Query,
 } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
 @Controller('movies')
 export class MoviesController {
-  @Get()
-  getAll() {
-    return 'This will return all movies';
-  }
+  constructor(private readonly moviesService: MoviesService) {}
 
-  @Get('search')
-  search(@Query('year') searchingYear: string) {
-    return `We are searching for a movie made a year : ${searchingYear}`;
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   @Get(':id')
-  getOne(@Param('id') id: string) {
-    return `This will return on movie with the id: ${id}`;
+  getOne(@Param('id') id: string): Movie {
+    return this.moviesService.getOne(id);
   }
 
   @Post()
   create(@Body() movieData) {
-    return movieData;
+    return this.moviesService.createMovie(movieData);
   }
 
   @Delete(':id')
   remove(@Param('id') movieId: string) {
-    return `this will delete a movie with the id: ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
